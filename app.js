50fdaada19efc7b8c30a8d0f99f853bbb4ff6423
Kay <kayathiri.r@gmail.com>
2018-02-14 10:00:39 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const request = require('request')
+const cors = require('cors')
+
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/terrier', function(req, res){
+var url = "https://dog.ceo/api/breed/terrier/list";
+ // request module is used to process the url and return the results in JSON format
+ request(url, function(err, resp, body) {
+   body = JSON.parse(body);
+   if (!body) {
+     body = "No results found. Try again.";
+     res.send(body);
+   }
+   else{
+     res.send(body.message)
+   }
+ });
+});
+
+app.listen(7000, () => console.log('Example app listening on port 7000!'))
