94696439a36f1139325db9ca2b565ba9a3b6fec6
Kay <kayathiri.r@gmail.com>
2018-02-14 13:04:04 -0600

    Updated verbose

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var corsOptions = {
 
 app.use(cors(corsOptions))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Backend Service: Please call /terrier!'))
 app.get('/terrier', function(req, res){
 var url = "https://dog.ceo/api/breed/terrier/list";
  // request module is used to process the url and return the results in JSON format
@@ -26,4 +26,4 @@ var url = "https://dog.ceo/api/breed/terrier/list";
  });
 });
 
-app.listen(7000, () => console.log('Example app listening on port 7000!'))
+app.listen(7000, () => console.log('Backend Service running on port 7000!'))
